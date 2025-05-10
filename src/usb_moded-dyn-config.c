b602c4013f850d1e16cf2f1acbd76d91f7770dbc
Dylan Van Assche <me@dylanvanassche.be>
2025-05-10 14:37:52 +0200

    [dyn-config] Add option for running a command on mode start/stop
    Allow specifying a command to execute on starting and stopping an USB mode
    such as a DHCP server, MTP responder, or another daemon which is needed
    for operating this USB mode. This feature complements DBus activation
    and systemd service activation.
    
    Signed-off-by: Dylan Van Assche <me@dylanvanassche.be>

--- src/usb_moded-dyn-config.c
+++ src/usb_moded-dyn-config.c
@@ -4,6 +4,7 @@
  * Copyright (c) 2011 Nokia Corporation. All rights reserved.
  * Copyright (c) 2013 - 2021 Jolla Ltd.
  * Copyright (c) 2020 Open Mobile Platform LLC.
+ * Copyright (c) 2025 Dylan Van Assche
  *
  * @author Philippe De Swert <philippe.de-swert@nokia.com>
  * @author Philippe De Swert <philippedeswert@gmail.com>
@@ -11,6 +12,7 @@
  * @author Thomas Perl <thomas.perl@jolla.com>
  * @author Slava Monich <slava.monich@jolla.com>
  * @author Simo Piiroinen <simo.piiroinen@jollamobile.com>
+ * @author Dylan Van Assche <me@dylanvanassche.be>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the Lesser GNU General Public License
@@ -100,6 +102,8 @@ modedata_free(modedata_t *self)
 #ifdef CONNMAN
         g_free(self->connman_tethering);
 #endif
+        g_free(self->ffs_daemon_start);
+        g_free(self->ffs_daemon_stop);
         modedata_flush_settings(self);
         free(self);
     }
@@ -151,6 +155,8 @@ modedata_copy(const modedata_t *that)
     self->cached_gateway             = g_strdup(that->cached_gateway);
     self->cached_nat_interface       = g_strdup(that->cached_nat_interface);
     self->cached_netmask             = g_strdup(that->cached_netmask);
+    self->ffs_daemon_start          = g_strdup(that->ffs_daemon_start);
+    self->ffs_daemon_stop           = g_strdup(that->ffs_daemon_stop);
 
 EXIT:
     return self;
@@ -275,6 +281,8 @@ modedata_load(const gchar *filename)
 #ifdef CONNMAN
     self->connman_tethering          = g_key_file_get_string(settingsfile,  MODE_OPTIONS_ENTRY, MODE_CONNMAN_TETHERING, NULL);
 #endif
+    self->ffs_daemon_start          = g_key_file_get_string(settingsfile,  MODE_OPTIONS_ENTRY, MODE_FFS_DAEMON_START, NULL);
+    self->ffs_daemon_stop           = g_key_file_get_string(settingsfile,  MODE_OPTIONS_ENTRY, MODE_FFS_DAEMON_STOP, NULL);
 
     //log_debug("Dynamic mode sysfs path = %s\n", self->sysfs_path);
     //log_debug("Dynamic mode sysfs value = %s\n", self->sysfs_value);
